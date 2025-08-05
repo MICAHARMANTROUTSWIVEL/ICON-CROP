3b66be01243922a3f11e90c4b9f23657be545dea
Armantrout, Micah <micah.armantrout@swbc.com>
2025-08-05 16:26:17 -0500

    fixing access and adding destroy

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,17 +1,22 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs'; 
+import { RemovalPolicy } from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
 export function createIconBucket(scope: Construct): {iconSource: s3.Bucket, iconDest: s3.Bucket} {
 
-    const iconSource = new s3.Bucket(scope, 'iconSource' )
+
+    const iconSource = new s3.Bucket(scope, 'iconSource', {
+        removalPolicy: RemovalPolicy.DESTROY
+    });
 
     const iconDest = new s3.Bucket(scope, 'iconDest', { 
+        removalPolicy: RemovalPolicy.DESTROY,
         blockPublicAccess:  new s3.BlockPublicAccess({ 
         blockPublicAcls: false, 
         ignorePublicAcls: false, 
         blockPublicPolicy: false }), 
-        publicReadAccess: false } )
+        publicReadAccess: true } )
 
         iconDest.addToResourcePolicy(
           new iam.PolicyStatement({
